d146879fe16ba2bf322516fe1c70111bcb4318e1
William R. Hogan <hoganwr@gmail.com>
2018-07-18 16:14:38 -0400

    added switch to command in runGeoNames... so that the thread dies nicely, with no Exceptions thrown

--- runGeoNamesAdminUnitProcessing.sh
+++ runGeoNamesAdminUnitProcessing.sh
@@ -1,1 +1,1 @@
-mvn exec:java -Dexec.mainClass="edu.ufl.bmi.ontology.GeoNamesAdminLevelProcessing" -Dexec.arguments="$1"
+mvn exec:java -Dexec.mainClass="edu.ufl.bmi.ontology.GeoNamesAdminLevelProcessing" -Dexec.arguments="$1" -Dexec.cleanupDaemonThreads=false
